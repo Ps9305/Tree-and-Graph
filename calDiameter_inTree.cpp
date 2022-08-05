da656ecd9de4a73f556aa62aebd8f62aa5f0b8f4
Prshant Singh <98302454+Ps9305@users.noreply.github.com>
2022-08-05 23:09:30 +0530

    Add files via upload

---
+++ calDiameter_inTree.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+struct node
+{
+    int data;
+    struct node* left;
+    struct node* right;
+    node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+int  calcheight(struct node* root){
+    if(root==NULL){
+        return 0;
+    }
+    int lHeight=calcheight(root->left);
+    int rHeight=calcheight(root->right);
+    return max(lHeight,rHeight)+1;
+}
+
+int calcDiameter(struct node* root){
+    if(root==NULL){
+        return 0;
+    }
+    int lHeight=calcheight(root->left);
+    int rHeight=calcheight(root->right);
+    int currDiameter=lHeight+rHeight+1;
+
+    int lDiameter=calcDiameter(root->left);
+    int rDiameter=calcDiameter(root->right);
+
+    return max(currDiameter,max(lDiameter,rDiameter));
+}
+int main(){
+    struct node* root=new node(1);
+    root->left=new node(2);
+    root->right=new node(3);
+
+    root->left->left=new node(4);
+    root->left->right=new node(5);
+    root->right->left=new node(6);
+    root->right->right=new node(7);
+    cout<<(root);
+    cout<<calcDiameter(root);
+    return 0;
+}
